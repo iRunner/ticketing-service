99c8c2a994facf041c46c0739ca4339a1a17bf9d
Abdullah Sahyouni <abdullahsahyouni@gmail.com>
2017-06-24 15:00:58 -0400

    Added swagger docs

--- src/main/java/ticketservice/TicketingController.java
+++ src/main/java/ticketservice/TicketingController.java
@@ -1,12 +1,13 @@
 package ticketservice;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class TicketingController {
 
-    @RequestMapping("/")
+    @RequestMapping(method = RequestMethod.GET, value = "/")
     public String index() {
         return "Greetings from Spring Boot!";
     }
